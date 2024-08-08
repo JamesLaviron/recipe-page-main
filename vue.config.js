86a05a0941397170818f93d329dbf7c90efd5269
JamesLaviron <victor@gerfaud.fr>
2024-08-08 16:35:00 +0200

    Reorganize project

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,24 @@
 const { defineConfig } = require('@vue/cli-service')
+const path = require('path')
+
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === "production" ? "/recipe-page-main/" : "/",
+  chainWebpack: config => {
+    const types = ['vue-modules', 'vue', 'normal-modules', 'normal']
+    types.forEach(type => addStyleResource(config.module.rule('stylus').oneOf(type)))
+  },
 })
+
+function addStyleResource (rule) {
+    rule.use('style-resource')
+      .loader('style-resources-loader')
+      .options({
+        patterns: [
+          path.resolve(__dirname, './src/styles/base.scss'),
+          path.resolve(__dirname, './src/styles/variables.scss'),
+          path.resolve(__dirname, './src/styles/typo.scss'),
+          path.resolve(__dirname, './src/styles/text-presets.scss'),
+        ],
+      })
+  }
