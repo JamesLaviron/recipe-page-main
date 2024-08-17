51861c6294ada6f5b5a08b2559234d6e1c45e781
JamesLaviron <victor@gerfaud.fr>
2024-08-17 20:07:54 +0200

    Fix most of style issues - still need some fices

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,142 @@
 <template>
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <main>
+    <article class="card">
+      <img
+        src="./assets/images/omelette.jpeg"
+        alt="Omelette"
+        width="800"
+        height="533"
+        class="card__image"
+      />
+      <div class="card__content">
+        <header class="card__header">
+          <h1 class="card__title text-preset-1">Simple Omelette Recipe</h1>
+          <p class="card__description text-preset-4">
+            An easy and quick dish, perfect for any meal. This classic omelette
+            combines beaten eggs cooked to perfection, optionally filled with
+            your choice of cheese, vegetables, or meats.
+          </p>
+        </header>
+        <section class="card__section card__section--preparation-time">
+          <h2
+            class="card__section-title card__section-title--preparation-time text-preset-3"
+          >
+            Preparation time
+          </h2>
+          <ul class="card__list">
+            <li class="card__list-item card__list-item--total-time">
+              <strong class="card__strong">Total:</strong> Approximately 10
+              minutes
+            </li>
+            <li class="card__list-item card__list-item--preparation-time">
+              <strong class="card__strong">Preparation:</strong> 5 minutes
+            </li>
+            <li class="card__list-item card__list-item--cooking-time">
+              <strong class="card__strong">Cooking:</strong> 5 minutes
+            </li>
+          </ul>
+        </section>
+        <section class="card__section card__section--ingredients">
+          <h2 class="card__section-title">Ingredients</h2>
+          <ul class="card__list">
+            <li class="card__list-item card__list-item--ingredient">
+              2-3 large eggs
+            </li>
+            <li class="card__list-item card__list-item--ingredient">
+              Salt, to taste
+            </li>
+            <li class="card__list-item card__list-item--ingredient">
+              Pepper, to taste
+            </li>
+            <li class="card__list-item card__list-item--ingredient">
+              1 tablespoon of butter or oil
+            </li>
+            <li class="card__list-item card__list-item--ingredient">
+              Optional fillings: cheese, diced vegetables, cooked meats, herbs
+            </li>
+          </ul>
+        </section>
+        <section class="card__section card__section--instructions">
+          <h2 class="card__section-title">Instructions</h2>
+          <ol class="card__list card__list--ordered">
+            <li class="card__list-item card__list-item--instruction">
+              <strong class="card__strong-instruction">Beat the eggs: </strong>
+              In a bowl, beat the eggs with a pinch of salt and pepper until
+              they are well mixed. You can add a tablespoon of water or milk for
+              a fluffier texture.
+            </li>
+            <li class="card__list-item card__list-item--instruction">
+              <strong class="card__strong-instruction">Heat the pan:</strong>
+              Place a non-stick frying pan over medium heat and add butter or
+              oil.
+            </li>
+            <li class="card__list-item card__list-item--instruction">
+              <strong class="card__strong-instruction"
+                >Cook the omelette:
+              </strong>
+              Once the butter is melted and bubbling, pour in the eggs. Tilt the
+              pan to ensure the eggs evenly coat the surface.
+            </li>
+            <li class="card__list-item card__list-item--instruction">
+              <strong class="card__strong-instruction"
+                >Add fillings (optional):
+              </strong>
+              When the eggs begin to set at the edges but are still slightly
+              runny in the middle, sprinkle your chosen fillings over one half
+              of the omelette.
+            </li>
+            <li class="card__list-item card__list-item--instruction">
+              <strong class="card__strong-instruction">Fold and serve:</strong>
+              As the omelette continues to cook, carefully lift one edge and
+              fold it over the fillings. Let it cook for another minute, then
+              slide it onto a plate.
+            </li>
+            <li class="card__list-item card__list-item--instruction">
+              <strong class="card__strong-instruction">Enjoy:</strong> Serve
+              hot, with additional salt and pepper if needed.
+            </li>
+          </ol>
+        </section>
+        <section class="card__section card__section--nutrition">
+          <h2 class="card__section-title text-preset-2">Nutrition</h2>
+          <p class="card__description text-preset-4">
+            The table below shows nutritional values per serving without the
+            additional fillings.
+          </p>
+          <table class="card__table">
+            <tbody class="card__table-body">
+              <tr class="card__table-row card__table-row--value">
+                <th class="card__table-header text-preset-4">Calories</th>
+                <td class="card__table-data text-preset-4">277kcal</td>
+              </tr>
+              <tr class="card__table-row card__table-row--value">
+                <th class="card__table-header text-preset-4">Carbs</th>
+                <td class="card__table-data text-preset-4">0g</td>
+              </tr>
+              <tr class="card__table-row card__table-row--value">
+                <th class="card__table-header text-preset-4">Protein</th>
+                <td class="card__table-data text-preset-4">20g</td>
+              </tr>
+              <tr class="card__table-row card__table-row--value">
+                <th class="card__table-header text-preset-4">Fat</th>
+                <td class="card__table-data text-preset-4">22g</td>
+              </tr>
+            </tbody>
+          </table>
+        </section>
+      </div>
+    </article>
+  </main>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
   name: "App",
-  components: {
-    HelloWorld,
-  },
+  components: {},
 };
 </script>
 
-<style>
+<style lang="scss">
 @import "./assets/styles/base.scss";
 @import "./assets/styles/main.scss";
 @import "./assets/styles/text-presets.scss";
--- vue.config.js
+++ vue.config.js
@@ -1,21 +1,15 @@
 const { defineConfig } = require('@vue/cli-service')
-const path = require('path')
 
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === "production" ? "/recipe-page-main/" : "/",
-  chainWebpack: config => {
-    const types = ['vue-modules', 'vue', 'normal-modules', 'normal']
-    types.forEach(type => addStyleResource(config.module.rule('scss').oneOf(type)))
-  },
-})
-
-function addStyleResource (rule) {
-    rule.use('style-loader', 'css-loader', 'sass-loader')
-      .loader('style-resources-loader')
-      .options({
-        patterns: [
-          path.resolve(__dirname, './src/assets/styles/variables.scss'),
-        ],
-      })
+  css: {
+    loaderOptions: {
+      scss: {
+        additionalData: `
+          @import "./assets/styles/_variables.scss";
+        `
+      }
+    }
   }
+})
